89e58176af3efdbc2e30709ada91485e12e53954
Aaron Drobek <adrobek09@gmail.com>
2017-08-10 15:18:51 -0400

    3:00 commit

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 let login = `
-<div class="login">
+  <div class="login">
 
 
 
-    <header class="signUpHeader">
-      <h1>Iron Meetups</h1>
-      <h4>Please Login!</h4>
-    </header>
-    <div class="loginContainer">
-      <input type="text" id="username" value="Username">
-      <input type="text" id="password" value="Password">
-      <button type="button" id="goToSignUp">Sign Up</button>
-      <button type="button" id="login" >Login</button>
+      <header class="signUpHeader">
+        <h1>Iron Meetups</h1>
+        <h4>Please Login!</h4>
+      </header>
+      <div class="loginContainer">
+        <input type="text" id="username" value="Username">
+        <input type="text" id="password" value="Password">
+        <button type="button" id="login" >Login</button>
+        <button type="button" id="goToSignUp">Create An Account</button>
+
+      </div>
 
 
-    </div>
-
-
-</div>`;
+  </div>`;
 
 let signUp = `
-<div class= "signup">
-<header class="signUpHeader">
-  <h1>Iron Meetups</h1>
-  <h4>Please Sign Up!</h4>
-</header>
-<div class="signupContainer">
-  <input type="text" id="username" value="Username">
-  <input type="text" id="password" value="Password">
-  <input type="text" id="confirm" value="Confirm Password">
-  <button type="button" id="signUp">Sign Up</button>
-  <button type="button" id="goToLogin">Login</button>
+  <div class= "signup">
+  <header class="signUpHeader">
+    <h1>Iron Meetups</h1>
+    <h4>Please Sign Up!</h4>
+  </header>
+  <div class="signupContainer">
+    <input type="text" id="username" value="Username">
+    <input type="text" id="password" value="Password">
+    <input type="text" id="confirm" value="Confirm Password">
+    <button type="button" id="signUp">Sign Up</button>
+    <button type="button" id="goToLogin">Already Have an Account? Click Here!</button>
 
-</div>
+  </div>
 
-</div>
+  </div>
 `;
 
 let eventList = `
@@ -71,61 +70,47 @@ let eventList = `
 
 let eventView = `
 
-<div class="eventView">
-      <header class="eventViewHeader">
-          <h2>Iron Meetups</h2>
-              <nav class="navTags">
-                <a class="link" href="eventlist.html">Events</a>
-                <a class="link" href="signup.html">Log Out</a>
-              </nav>
-      </header>
+  <div class="eventView">
+        <header class="eventViewHeader">
+            <h2>Iron Meetups</h2>
+                <nav class="navTags">
+                  <a class="link" href="eventlist.html">Events</a>
+                  <a class="link" href="signup.html">Log Out</a>
+                </nav>
+        </header>
 
-      <div class="eventViewContainer">
-        <div class="eventDescription">
-              <h1>Event Name</h1>
-              <h2>When?</h2>
-              <p id="eventDateTime"> "date" at "time"</p>
-              <h2>Where?</h2>
-              <p id="eventLocation">"location"</p>
-              <h2>What is this all about?</h2>
-              <p id="eventDescription">"event description"</p>
-        </div>
+        <div class="eventViewContainer">
+          <div class="eventDescription">
+                <h1>Event Name</h1>
+                <h2>When?</h2>
+                <p id="eventDateTime"> "date" at "time"</p>
+                <h2>Where?</h2>
+                <p id="eventLocation">"location"</p>
+                <h2>What is this all about?</h2>
+                <p id="eventDescription">"event description"</p>
+          </div>
 
-        <div class="attendeesList">
-              <h2>Attendees</h2>
-              <p id="attendees"></p>
-       <!-- //will be in for loop to generate registered users. Wasn't sure if p tag was most appropriate. -->
-        </div>
-</div>
+          <div class="attendeesList">
+                <h2>Attendees</h2>
+                <p id="attendees"></p>
+         <!-- //will be in for loop to generate registered users. Wasn't sure if p tag was most appropriate. -->
+          </div>
+  </div>
 `;
+ function signup() {
+   let goToSignUp = document.querySelector("#goToSignUp");
 
+   goToSignUp.addEventListener('click', function(){
+     console.log("signupActivated");
+     container.innerHTML= signUp;
+   });
+ }
 let container = document.querySelector(".container");
 container.innerHTML= login;
-	console.log("working");
-
-function backToSignUp() {
+console.log("working");
 
 
-			if(container.innerHTML = login){
-				let goToSignUp = document.querySelector("#goToSignUp");
-				goToSignUp.addEventListener('click', function(){
-							console.log("signupActivated");
-							container.innerHTML= signUp;
-							backToLogIn();
-				})
-}
-}
-function backToLogIn(){
-				 if(container.innerHTML = signUp){
-					let goToLogin = document.querySelector("#goToLogin");
-						goToLogin.addEventListener('click', function(){
-						console.log("loginWorking");
-						container.innerHTML= login;
-						backToSignUp();
+let goToLogin = document.querySelector("#goToLogin");
+goToLogin.addEventListener('click', function(){
 
-
-					});
-
-
-};
-}
+});
