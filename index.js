5edf87a72c3dd75eac7095e9666b9d1024194959
WilliamRausch <nolanrausch@Gmail.com>
2017-08-10 12:47:17 -0400

    commit

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 //examples below
-window.sessionStorage
+console.log("LINKED");
+//window.sessionStorage
 
-console.log("it's working");
-if (window.sessionStorage.accesspasstokenname)
- //loaddefaulteventlistpage
- else{
-     //load the login page
- }
+// console.log("it's working");
+// if (window.sessionStorage.accesspasstokenname)
+//  //loaddefaulteventlistpage
+//  else{
+//      //load the login page
+//  }
+let iframe = document.getElementById("#theFrame");
+console.log("the frams is" + iframe);
+let goToSignUp = document.querySelector('#goToSignUp');
+goToSignUp.addEventListener('click',function(){
+	console.log("hello");
+	console.log(iframe);
+	iframe.setAttribute("src", "./views/signup.html");
+});
